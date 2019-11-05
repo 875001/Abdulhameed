deaff13383c34357337db0aa8998c42094f73e19
Abdulhameed <875001@pdsb.net>
2019-11-05 09:12:20 -0500

    fixing assignment 2 buttons

---
+++ script.js
@@ -0,0 +1,31 @@
+function angle3(){
+    alert("give me the two angles and I will give you the third angle"); 
+    let angle1=prompt("Enter angle 1");
+    let angle2=prompt("Enter angle 2");
+    let angle3=180- angle1 - angle2;
+    alert("the 3rd angle is" + angle3);
+}
+
+function test(){
+    console.log("working!");
+}
+
+//  centryfromyear(95) returns (1)
+//  centryfromyear(150) returns (2)
+//  centryfromyear(295) returns (3)
+//  centryfromyear(305) returns (4)
+ 
+//  for(let i = 0; i < 101 ; i++){
+//      if(i % 10 == 0 && i % 4 == 0){
+//          console.log("tenFour");
+//      }
+//      else if(i % 4 == 0){
+//          console.log("Four");
+//      }
+//      else if(i % 10 == 0){
+//          console.log("Ten");
+//      }
+//      else {
+         
+//      }
+//  }
